47d2eddfbd23bd591a8ab73551a5db93dc63fac9
Anai <77222095+Anai-1@users.noreply.github.com>
2022-08-22 23:44:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-day = datetime.date.today
+day = datetime.date.today()
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
